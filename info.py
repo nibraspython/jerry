022a7be28511c2c4236229a307d1faacb8bb4640
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-27 20:06:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 import re
 from os import environ
 
-SESSION = environ.get('SESSION', 'small_bot')
-API_ID = int(environ.get('API_ID', '23381624'))
-API_HASH = environ.get('API_HASH', '02e5d155e1abcdb51673733ecc088a29')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7019442694:AAEPow32KsK9UMSx1btpKkE_TwGBqBExqQE')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001668201351'))
+SESSION = environ.get('SESSION', 'jerrybot')
+API_ID = int(environ.get('API_ID', '23648338'))
+API_HASH = environ.get('API_HASH', '9b23c1dda0eaf6c48758d4c1e6ae9fe0')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6926402548:AAECdcA1FcyxGfDWlBZbtAFYLY_DF19NI8g')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001771073047')
 PORT = environ.get("PORT", "8080")
