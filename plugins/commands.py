75b50ecce68c0abba6c11750b14f648fc92f25d7
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-27 22:32:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,14 +1,35 @@
 from Script import script
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters, enums
-from info import PICS
+from info import PICS, ADMINS, LOG_CHANNEL
 import os, random, asyncio
 
 CMD = ["/", "."]
 
+TEXT_TXT = """<b> Hᴇʟʟᴏ {}.
+
+Mʏ Nᴀᴍᴇ Is {}.
+
+I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Oʀ Jᴏɪɴ Oᴜʀ Gʀᴏᴜᴘ 🫶🏻🤍</b>"""
+
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if user_id in ADMINS: return # ignore admins
+    await message.reply_text(
+         text="<b>ʜᴇʏ ᴅᴜᴅᴇ 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ᴏɴ ᴏᴜʀ <a href=https://t.me/CINEMA1123>ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a> ᴏʀ ᴄʟɪᴄᴋ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ👇</b>",   
+         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ", url=f"t.me/Cinema1123")]]))
+    await bot.send_message(
+        chat_id=LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+    )
+    
 @Client.on_message(filters.command("start"))
 async def start(client, update):
-    await update.reply_photo(photo=random.choice(PICS), caption=script.TEXT_TXT.format(update.from_user.mention), reply_markup=BUTTONS, quote=True)
+    await update.reply_photo(photo=random.choice(PICS), caption=TEXT_TXT.format(update.from_user.mention), reply_markup=BUTTONS, quote=True)
 
 TEXT = """**Hai {},
 I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).**
@@ -65,8 +86,8 @@ async def help(_, message):
         buttons = [[
                     InlineKeyboardButton('⛦ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ⛦', url=f'http://t.me/oggyRbot?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🌐 Aʙᴏᴜᴛ', callback_data="about"),
-                    InlineKeyboardButton('⚙️ Dᴏɴᴀᴛᴇ', url='https://t.me/xax_ha_ha_l')
+                    InlineKeyboardButton('⚙️ ꜰᴜɴᴛɪᴏɴ ⚙️', callback_data="/start"),
+                    InlineKeyboardButton('🧭 ᴀʙᴏᴜᴛ 🧭', url='https://t.me/xax_ha_ha_l')
                 ],[
                     InlineKeyboardButton('🕸️ Hᴇʟᴩ', callback_data="start")
                   ]]
