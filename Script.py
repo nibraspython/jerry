8373942839abee19264600fc1f04027978db8af1
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-27 21:33:41 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,20 @@
+class script(object):  
+    TEXT_TXT = """<b> Hᴇʟʟᴏ {user}.
+
+Mʏ Nᴀᴍᴇ Is {bot}.
+
+I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Oʀ Jᴏɪɴ Oᴜʀ Gʀᴏᴜᴘ 🫶🏻🤍</b>"""
+    
+    HELP_TXT = "Hᴇʏ {}\nHᴇʀᴇ Mꜱ Mʏ Hᴇʟᴩ"
+
+    ABOUT_TXT = """<b> Mʏ Nᴀᴍᴇ: {}
+    
+👦🏼 Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/xax_ha_ha_l'>𝙾𝙶𝙶𝚈 𝚇 𝙹𝙴𝚁𝚁𝚈</a>
+
+📡 Bᴏᴛ Sᴇʀᴠᴇʀ: <a href='https://t.me/NibrasHacked'>𝙿𝚁𝙸𝚅𝙰𝚃𝙴_𝚅𝙿𝚂 📟</a></b>"""
+   
+    SOURCE_TXT = """<b>🪧 ɴᴏᴛᴇ:
+- ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴩʀɪᴠᴀᴛᴇ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
+- ꜱᴏᴜʀᴄᴇ - <a href="https://t.me/xax_ha_ha_l">Pᴍ Mᴇ 📩</a>
+⚙️ Dᴇᴠᴇʟᴏᴘᴇʀ:
+- <a href="https://t.me/xax_ha_ha_l">𝙾𝙶𝙶𝚈 𝚇 𝙹𝙴𝚁𝚁𝚈</a></b>"""
