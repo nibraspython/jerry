f54ce74fbea6a0c939cf2cb01354b2f67c6c8e46
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-27 20:18:50 +0530

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,16 @@
+import time
+import random
+import asyncio
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, filters, enums
+from Script import script
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...........")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"𝖯𝗂𝗇𝗀!\n{time_taken_s:.3f} ms")
