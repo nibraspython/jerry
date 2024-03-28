21bda10ca13c22aaaa8c631ddd8bbfde78047967
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-28 22:34:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -102,23 +102,23 @@ async def pm_text(bot, message):
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "file":
-          buttons = [[
+            buttons = [[
             InlineKeyboardButton(text="𝖶𝗂𝗍𝗁 𝖶𝗁𝗂𝗍𝖾 𝖡𝖦", callback_data="rmbgwhite"),
             InlineKeyboardButton(text="𝖶𝗂𝗍𝗁𝗈𝗎𝗍 𝖡𝖦", callback_data="rmbgplain"),
             ],[
             InlineKeyboardButton(text="𝖲𝗍𝗂𝖼𝗄𝖾𝗋", callback_data="rmbgsticker"),
             ],[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
-        ]]
+            ]]
         await query.message.edit_text("**Select Required Mode**", reply_markup=InlineKeyboardMarkup(buttons))
-            
+           
     elif query.data == "stick":
-        buttons = [[
+            buttons = [[
             InlineKeyboardButton(text="𝖭𝗈𝗋𝗆𝖺𝗅", callback_data="stkr"),
             InlineKeyboardButton(text="𝖤𝖽𝗀𝖾 𝖢𝗎𝗋𝗏𝖾𝖽", callback_data="cur_ved"),
             ],[                    
             InlineKeyboardButton(text="𝖢𝗂𝗋𝖼𝗅𝖾", callback_data="circle_sticker")
             ],[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
-        ]]              
+            ]]              
         await query.message.edit("**Select A Type**", reply_markup=InlineKeyboardMarkup(buttons))
