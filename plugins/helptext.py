1355304dc76ce958e437caa2f8c37f59759fb84a
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-04-05 12:18:36 +0530

    Create helptext.py

---
+++ plugins/helptext.py
@@ -0,0 +1,39 @@
+from info import help_message
+
+help_message.append({"Module_Name": "fun"}, )
+
+help_message.append({"Module_Name": "telegraph"})
+
+
+class Help_Text:
+    helpp = {
+        "admin_help": """
+• /ban - __reply to a user__
+• /unban __user id or username__
+• /pin - __reply to a message__
+• /unpin - __reply to a message__
+• /id - __to get the user id__
+• /purge - __purge the messages__
+
+""",
+        "meme_help": """
+• /rmeme - __to get random anime memes__
+""",
+        "notes_help":
+        '• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__',
+        "extra_help": """• /watchorder __anime name - to get watchorder__
+• /quote - __to get random anime quotes__
+• /pat - __ to pat someone__
+• /invitelink - __get invitelink of the current chat__
+• /whatanime - __reply to a gif or video__
+• /wall your query - __wallpapers__ 
+• /paste - __reply to a message__
+• define word - __get meaning of a word__
+
+""",
+        "welcome_help": """• /setwelcome __welcome message - text for welcome__
+• /clearwelcome - __clear welcome message__""",
+        "fun_help": """•/eye
+•/run""",
+        "telegraph_help": """• /ul - __Reply to a photo or video__""",
+    }
