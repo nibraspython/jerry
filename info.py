5f108bdff1c92cc2bec4e9eff6de31c583782420
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-12-16 14:31:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,16 +13,16 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get('SESSION', 'jerrybot')
-API_ID = int(environ.get('API_ID', '23648338'))
+API_ID = int(environ.get('API_ID', '27761739'))
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://OGGYYTT:OGGYYTT@cluster0.yfmak7z.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "oggytg")
-API_HASH = environ.get('API_HASH', '9b23c1dda0eaf6c48758d4c1e6ae9fe0')
+API_HASH = environ.get('API_HASH', '68a5e625d16fc3759cddda0264f2110e')
 PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
-BOT_TOKEN = environ.get('BOT_TOKEN', '7191755037:AAETjjIHi0dcWBpvaR5LZk9fz3mO_x5IvbM')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7582317858:AAEN3wjYk-1UBKXbQfMb2PpRn8_E3zrk1ik')
 MSG_ALRT = environ.get('MSG_ALRT', 'You Fool!? 😩')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001938307351'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002328276546'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 PORT = environ.get("PORT", "8080")
 
